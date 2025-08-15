dcf2bd22777a996b913fc1885f1bddd0bee7a24a
luisbr83 <jonathaspt1983@gmail.com>
2025-08-15 14:15:22 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,6 +49,19 @@ export default function RootLayout({
           async
           defer
         ></Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "sv8dsqnfqg");
+          `}
+        </Script>
+        {/* End Microsoft Clarity */}
+        
       </head>
       <body className="font-body antialiased">
         {children}
